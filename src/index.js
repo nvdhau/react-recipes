e8838c070e5e55db45226210602c1cc13c9f1d80
VuDuyHauNguyen <nguyenv19@student.douglascollege.ca>
2019-10-30 18:49:40 -0700

    mount root component with react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import log from './log.js';
+import React  from "react";
+import ReactDOM from "react-dom";
+import App from './App.js';
 
-log("Hello World!!!");
+const MOUNT_ELEMENT = document.getElementById('root');
+
+ReactDOM.render(<App />, MOUNT_ELEMENT);
