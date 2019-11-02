3ce143226b6539be638b4b4d0dff943760d7ef62
VuDuyHauNguyen <nguyenv19@student.douglascollege.ca>
2019-11-02 14:13:42 -0700

    add Redirect /home to /

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import Favorites from "./Favorites";
@@ -12,6 +12,7 @@ const App = () => {
         <Header />
 
         <Switch>
+          <Redirect from="/home" to="/" />
           <Route exact path="/" component={Home} />
           <Route path="/favorites" component={Favorites} />
           <Route component={NotFound} />
