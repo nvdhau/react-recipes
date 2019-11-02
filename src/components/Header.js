b7864a052461c854bdf57d290f73b53a6f0d016b
VuDuyHauNguyen <nguyenv19@student.douglascollege.ca>
2019-11-02 14:30:53 -0700

    add NavLink from react-router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+
+// activeClassName from react-router-dom
+
+const HeaderLink = ({ children, ...props }) => (
+  <NavLink
+    exact
+    to="/"
+    className="p1 mx2 black rounded text-decoration-none"
+    activeClassName="bg-white"
+    {...props}
+  >
+    {children}
+  </NavLink>
+);
+
+HeaderLink.propTypes = {
+  children: PropTypes.node
+};
 
 const Header = () => (
-  <header className="px4">
+  <header className="flex items-center justify-between px4">
     <h1 className="h1">My Recipes</h1>
+    <nav>
+      <HeaderLink to="/">Home</HeaderLink>
+      <HeaderLink to="/favorites">Favorites</HeaderLink>
+    </nav>
   </header>
 );
 
